c06cb7994ce3e3f5b45ee119e2e3da608dac5bba
INFINIX <72700816+DevInfinix@users.noreply.github.com>
2024-06-19 15:08:17 +0530

    update country, state and region

--- signing-derp.sh
+++ signing-derp.sh
@@ -1,4 +1,4 @@
-subject='/C=US/ST=State/L=City/O=Android/OU=Android/CN=Android/emailAddress=email@example.com';
+subject='/C=IN/ST=Maharashtra/L=Mumbai/O=Android/OU=Android/CN=Android/emailAddress=email@example.com';
 for x in releasekey platform shared media networkstack verity otakey testkey sdk_sandbox bluetooth;
 do ./development/tools/make_key vendor/derp/signing/keys/$x "$subject";
 done
